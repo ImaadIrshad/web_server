e7fb32d62ef6400ad2345e872696576b77f53308
Imaad <53949355+ImaadIrshad@users.noreply.github.com>
2021-12-05 00:35:05 +0400

    Update

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+
+ app.get('', (req, res) => {
+    res.send('<h1>Weather<h1>')
+ })
+
+ app.get('/help', (req, res) => {
+    res.send({
+        name: 'Imaad',
+        age: 23
+    })
+ })
+
+ app.get('/about', (req, res) => {
+     res.send('<h1>About</h1>')
+ })
+
+ app.get('/weather', (req, res) =>{
+     res.send( {
+         forecast: 'It is windy',
+         location: 'Dubai'
+     })
+ })
+
+ app.listen(3000), () => {
+    console.log('Server is up on port 3000')
+ }

92598b56741853cb0eb82a9a71cf98dc1575db86
Imaad <53949355+ImaadIrshad@users.noreply.github.com>
2021-12-13 20:03:05 +0400

    Update

--- src/app.js
+++ src/app.js
@@ -1,29 +1,67 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const { title } = require('process')
 
 const app = express()
 
- app.get('', (req, res) => {
-    res.send('<h1>Weather<h1>')
- })
+// Define paths for Express configuration
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
- app.get('/help', (req, res) => {
-    res.send({
-        name: 'Imaad',
-        age: 23
+// Setup handlebars engine and views location (instead of .html)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory location 
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) =>{
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Imaad'
     })
- })
+})
 
- app.get('/about', (req, res) => {
-     res.send('<h1>About</h1>')
- })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Imaad'
+    })
+})
 
- app.get('/weather', (req, res) =>{
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Section',
+        name: 'Imaad Irshad'
+    })
+})
+
+app.get('/weather', (req, res) =>{
      res.send( {
          forecast: 'It is windy',
          location: 'Dubai'
      })
  })
 
- app.listen(3000), () => {
-    console.log('Server is up on port 3000')
+ app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Imaad Irshad',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Imaad Irshad',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000), () => {
+    console.log("Server is up on port 3000")
  }
